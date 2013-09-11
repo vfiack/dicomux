5e5a5185b93c574938d710c4796e9d90e3be3cd7
Vincent Fiack <vfiack@gmail.com>
2013-09-11 15:20:15 +0200

    stroke control

--- src/main/java/dicomux/waveform/DrawingPanel.java
+++ src/main/java/dicomux/waveform/DrawingPanel.java
@@ -302,8 +302,7 @@ public class DrawingPanel extends JPanel {
 		// set rendering options
 		g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);    
 		g2.setRenderingHint(RenderingHints.KEY_RENDERING, RenderingHints.VALUE_RENDER_QUALITY);
-		g2.setRenderingHint(RenderingHints.KEY_STROKE_CONTROL, RenderingHints.VALUE_STROKE_NORMALIZE);
-		g2.setRenderingHint(RenderingHints.KEY_DITHERING, RenderingHints.VALUE_DITHER_ENABLE);
+		g2.setRenderingHint(RenderingHints.KEY_STROKE_CONTROL, RenderingHints.VALUE_STROKE_PURE);
 							
 		this.dim = getPreferredSize();
 		// calculate height and width of the cells
